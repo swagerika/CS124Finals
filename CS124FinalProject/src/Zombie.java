ae1a6a6ea4e3d1162ca68949ca3ca6edb201b3ae
Carlo Luis De Guzman <carlodeguzman5@gmail.com>
2015-03-12 14:17:46 +0800

    Updated Zombie and State Classes

--- CS124FinalProject/src/Zombie.java
+++ CS124FinalProject/src/Zombie.java
@@ -4,13 +4,15 @@ import java.awt.Graphics;
 public abstract class Zombie {
 	private ZombieFlyWeight source;
 	private int size;
+	private State state;
 
 	public void setSource(ZombieFlyWeight s){
 		source = s;
 		size = source.getSize();
+		state = null;
 	}
 	public int getSize(){
-		return size; 
+		return size;  
 	}
 	
 	public abstract void draw(Graphics g);
